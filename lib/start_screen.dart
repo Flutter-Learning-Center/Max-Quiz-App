97c8763d29551b5fdc7e1a72cf975d04f06242c2
Sławomir Oruba <slawomir.oruba@gmail.com>
2024-02-04 10:13:05 +0100

    first commit

---
+++ lib/start_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class StartScreen extends StatelessWidget {
+  const StartScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset('assets/images/quiz-logo.png', width: 250),
+          const SizedBox(height: 50),
+          const Text('Learn Flutter the fun way!',
+              style: TextStyle(fontSize: 20, color: Colors.white)),
+          const SizedBox(height: 50),
+          ElevatedButton(
+            onPressed: () {
+              print('Button pressed');
+            },
+            child: const Text('Start Quiz'),
+          ),
+        ],
+      ),
+    );
+  }
+}
